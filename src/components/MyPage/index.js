cbc23705b4f6b953b552689520ec87451cdad551
KoheiAsano <komerumeru@i.softbank.jp>
2019-03-15 11:20:49 +0900

    fix directory components

--- src/components/MyPage/index.js
+++ src/components/MyPage/index.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 
+const books = ["ゼロから作るDeep_Lerning", "PRML","やさしいJAVA", "アナログ電子回路の基礎", "Linux_Bible" ];
+
+
+
 class MyPage extends Component {
   render() {
     return (
-      <div className="App">
-        <p>MyPage</p>
+      <div>
+        <div><MyPageTitle /></div>
+        {books.map((book,i) => {
+          return (
+            //<p><button onClick={() => console.log("Clicked" + book)}> {book} </button></p>
+            <p><BookLink bookName={book}/></p>
+          )
+        })}
+        <div>message = {'ふがふが'}</div>
+      </div>
+    );
+  }
+}
+
+class MyPageTitle extends Component {
+  render() {
+    return (
+      <div>
+        <p>マイページ</p>
+      </div>
+    );
+  }
+}
+class BookLink extends Component {
+  render() {
+    return (
+      <div>
+        <p><button onClick={() => alert('clicked')}> {this.props.bookName} <br/>[著者]<br/>[本の画像がここに表示される]</button></p>
       </div>
     );
   }
