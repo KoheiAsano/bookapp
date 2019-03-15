1ee06fbe1b5e347a10e0146d7f0f23d4d085836b
KoheiAsano <komerumeru@i.softbank.jp>
2019-03-15 14:37:58 +0900

    create comment modal

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
-
+import CommentModal from './CommentModal';
 class Comment extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      Comment:false,
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img  className="App-logo" alt="logo" />
-          <p>
-            Comment
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            <p>
+              Comment
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
         </header>
+        <button onClick={() => this.setState({Comment:true})}>Comment</button>
+        <CommentModal CommentModalOpen={this.state.Comment} onCommentModalClose={() => this.setState({Comment: false})}/>
       </div>
     );
   }
