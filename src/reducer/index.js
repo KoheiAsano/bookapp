86ec120aa77a279f88ca83882c4a9a8ddb59f9de
KoheiAsano <komerumeru@i.softbank.jp>
2019-03-15 13:17:27 +0900

    create auth reducer

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
+import auth from './auth';
 import books from './books';
 import bookComments from './bookComments'
 import onebook from './onebook';
+
 export default combineReducers({
+  auth,
   books,
   bookComments,
   onebook,
